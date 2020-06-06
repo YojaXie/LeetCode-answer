2c0d3b949c791b29053fcfe72ee810c6aef77cb7
也佳 谢 <450542516@qq.com>
2020-06-06 20:15:59 +0800

    已完成

--- 探索数组和字符串/两数之和II.cpp
+++ 探索数组和字符串/两数之和II.cpp
@@ -26,12 +26,24 @@ using namespace std;
 class Solution 
 {
 public:
-	vector<int> twoSum(vector<int>& numbers, int target) 
+	vector<int> twoSum(vector<int>& numbers, int target)
 	{
-		int half = target/2;
-		for (int i = 0; i < numbers.size(); i++)
+		vector<int> res;
+		int i = 0;
+		int j = numbers.size() - 1;
+		while (i < j)
 		{
-
+			if (numbers[j] + numbers[i] == target)
+			{
+				res.push_back(i + 1);
+				res.push_back(j + 1);
+				break;
+			}
+			else if (numbers[j] + numbers[i] < target)
+				i++;
+			else 
+				j--;
 		}
+		return res;
 	}
 };
