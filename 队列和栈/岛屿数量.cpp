1ffd3c3a8adc9cc278068c7d5b7c445a18ac5880
也佳 谢 <450542516@qq.com>
2020-06-10 17:04:49 +0800

    已完成

--- 队列和栈/岛屿数量.cpp
+++ 队列和栈/岛屿数量.cpp
@@ -37,40 +37,59 @@ class Solution
 public:
 	int numIslands(vector<vector<char>>& grid) 
 	{
-		if (grid.size())
+		if (grid.empty())
 			return 0;
 		int nums = grid.size() * grid[0].size();
 		int res = 0;
-		queue<char*> queueIslands;
-		set<char*> used;
-		for (vector<vector<char>>::iterator it = grid.begin(); it != grid.end(); it++)
+		queue<pair<int, int>> queueIslands;
+		set<pair<int, int>> used;
+		int nr = grid.size();
+		int nc = grid[0].size();
+		for (int i = 0; i < nr; i++)
 		{
-			for (int i = 0; i < (*it).size(); i++)
+			for (int j = 0; j < nc; j++)
 			{
 				if (used.size() == nums)
 					return res;
-				int step = 0;
-				int* root = NULL;
-				queueIslands.push(&(*it)[i]);
-				used.insert(&(*it)[i]);
+				pair<int, int> root(i, j);
+				if (used.count(root))
+					continue;
+				used.insert(root);
+				if (grid[root.first][root.second]=='1')
+					res = res + 1;
+				else
+					continue;
+				queueIslands.push(root);
 				while (!queueIslands.empty())
 				{
-					step = step + 1;
-					int size = queueIslands.size();
-					for (int i = 0; i < size; i++)
+					int row = queueIslands.front().first, col = queueIslands.front().second;
+					queueIslands.pop();
+					if (grid[row][col]=='1')
 					{
-						if (queueIslands.front())
+						if (row - 1 >= 0 && used.count({ row - 1,col }) == 0)
 						{
-
-							for (int j = 0; j < grid[step - 1].size(); j++)
-							{
-
-							}
-
+							queueIslands.push({ row - 1,col });
+							used.insert({ row - 1,col });
+						}
+						if (row + 1 < nr && used.count({ row + 1,col }) == 0)
+						{
+							queueIslands.push({ row + 1,col });
+							used.insert({ row + 1,col });
+						}
+						if (col - 1 >= 0 && used.count({ row,col - 1 }) == 0)
+						{
+							queueIslands.push({ row,col - 1 });
+							used.insert({ row,col - 1 });
+						}
+						if (col + 1 < nc && used.count({ row,col + 1 }) == 0)
+						{
+							queueIslands.push({ row,col + 1 });
+							used.insert({ row,col + 1 });
 						}
 					}
 				}
 			}
 		}
+		return res;
 	}
 };
