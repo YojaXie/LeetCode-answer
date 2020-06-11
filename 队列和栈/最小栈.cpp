b6fb5dc2dac975cd6ff59c3495b492939e68940f
也佳 谢 <450542516@qq.com>
2020-06-11 11:53:26 +0800

    添加一题-最小栈（已完成）

---
+++ 队列和栈/最小栈.cpp
@@ -0,0 +1,88 @@
+/*
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+push(x) —— 将元素 x 推入栈中。
+pop() —— 删除栈顶的元素。
+top() —— 获取栈顶元素。
+getMin() —— 检索栈中的最小元素。
+
+示例:
+输入：
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+输出：
+[null,null,null,null,-3,null,0,-2]
+
+解释：
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+
+
+提示：
+pop、top 和 getMin 操作总是在 非空栈 上调用。
+*/
+
+
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class MinStack 
+{
+private:
+	vector<pair<int, int>> data;
+	int MinNum;
+
+public:
+	/** initialize your data structure here. */
+	MinStack() 
+	{
+		MinNum = INT_MAX;
+	}
+	void push(int x) 
+	{
+		if (data.empty())
+			data.push_back({ x, x });
+		else
+		{
+			MinNum = data.back().second;
+			MinNum = min(x, MinNum);
+			data.push_back({ x, MinNum });
+		}
+	}
+	void pop() 
+	{
+		if (!data.empty())
+			data.pop_back();
+	}
+	int top() 
+	{
+		if (!data.empty())
+			return data.back().first;
+		return -1;
+	}
+	int getMin() 
+	{
+		if (!data.empty())
+			return data.back().second;
+		return -1;
+	}
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
