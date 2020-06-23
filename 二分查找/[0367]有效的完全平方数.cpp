9437a5f381dc68ab458e8a5973f399b9fe34d494
也佳 谢 <450542516@qq.com>
2020-06-23 17:50:18 +0800

    添加一题-[0367]有效的完全平方数（已完成）

---
+++ 二分查找/[0367]有效的完全平方数.cpp
@@ -0,0 +1,36 @@
+﻿/*
+给定一个正整数 num，编写一个函数，如果 num 是一个完全平方数，则返回 True，否则返回 False。
+说明：不要使用任何内置的库函数，如  sqrt。
+
+示例 1：
+输入：16
+输出：True
+
+示例 2：
+输入：14
+输出：False
+*/
+
+#include<iostream>
+#include<algorithm>
+#include<stack>
+
+using namespace std;
+
+class Solution 
+{
+public:
+	bool isPerfectSquare(int num) 
+	{
+		int leftp = 1, rightp = num;
+		long int midp = 0;
+		while (leftp <= rightp)
+		{
+			midp = leftp + (rightp - leftp) / 2;
+			if (midp * midp > num) rightp = midp - 1;
+			else if (midp * midp < num) leftp = midp + 1;
+			else return true;
+		}
+		return false;
+	}
+};
