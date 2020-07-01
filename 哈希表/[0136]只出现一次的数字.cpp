911b15a7297b5a5a9d5fcc05ae4dc3b450938fc2
也佳 谢 <450542516@qq.com>
2020-07-01 18:02:36 +0800

    添加一题-[0136]只出现一次的数字（已完成）

---
+++ 哈希表/[0136]只出现一次的数字.cpp
@@ -0,0 +1,40 @@
+/*
+给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+说明：
+你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+示例 1:
+输入: [2,2,1]
+输出: 1
+
+示例 2:
+输入: [4,1,2,1,2]
+输出: 4
+*/
+
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<set>
+#include<unordered_map>
+#include<algorithm>
+#include<string>
+#include<unordered_set>
+
+using namespace std;
+
+class Solution 
+{
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        int res = 0;
+        for (auto num : nums)
+        {
+            res = res ^ num;
+        }
+        return res;
+    }
+};
