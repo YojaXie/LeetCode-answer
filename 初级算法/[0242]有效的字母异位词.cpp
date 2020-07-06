f6d272a5c0e41578f6f8bc0e0efa2e56f0afcc0d
也佳 谢 <450542516@qq.com>
2020-07-06 18:26:16 +0800

    添加一题-[0242]有效的字母异位词（已完成）

---
+++ 初级算法/[0242]有效的字母异位词.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<set>
+#include<unordered_map>
+#include<algorithm>
+#include<string>
+#include<unordered_set>
+#include<map>
+
+using namespace std;
+
+class Solution 
+{
+public:
+	bool isAnagram(string s, string t) 
+	{
+		sort(s.begin(), s.end());
+		sort(t.begin(), t.end());
+		if (s == t) return true;
+		else return false;
+	}
+};
