8e4d966de8f82c9f6bea03d15e6e1b8e237db573
也佳 谢 <450542516@qq.com>
2020-07-07 17:16:14 +0800

    添加一题-[0237]删除链表中的节点（已完成）

---
+++ 初级算法/[0237]删除链表中的节点.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<set>
+#include<unordered_map>
+#include<algorithm>
+#include<string>
+#include<unordered_set>
+#include<map>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode 
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution 
+{
+public:
+    void deleteNode(ListNode* node)
+    {
+        ListNode* del = node->next;
+        node->val = node->next->val; node->next = node->next->next;
+        delete del;
+    }
+};
