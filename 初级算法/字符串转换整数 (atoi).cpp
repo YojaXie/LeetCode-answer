6d89f44e9138a1ce8593ff8296420f366110f543
也佳 谢 <450542516@qq.com>
2020-07-07 09:16:58 +0800

    修改

---
+++ 初级算法/字符串转换整数 (atoi).cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<set>
+#include<unordered_map>
+#include<algorithm>
+#include<string>
+#include<unordered_set>
+#include<map>
+
+using namespace std;
+
+class Solution 
+{
+public:
+	int myAtoi(string str) 
+	{
+		if (str.empty()) return 0;
+		int res = 0;
+		string Num;
+		for (char chr : str)
+		{
+			if (chr == ' ') continue;
+			else if(chr)
+			{
+
+			} 
+		}
+	}
+};
